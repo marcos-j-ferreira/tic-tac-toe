5d68171bb666ba9922f30e0da8bb89e7666e862b
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-02-05 21:40:23 -0300

    print

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def v_vertical_01(table:list) -> str:
     x:int = 0
     bolinha:int = 0 
 
-    if table[0][0] == 1 and table[1][1] == 1 and table[2][2] == 1:
+    if table[0][0] == 'X' and table[1][1] == 'X' and table[2][2] == 'X':
         bolinha = 3
-    elif table[0][0] == 2 and table[1][1] == 2 and table[2][2] == 2:
+    elif table[0][0] == 'O' and table[1][1] == 'O' and table[2][2] == 'O':
         x = 3
     
     if bolinha == 3:
@@ -28,9 +28,9 @@ def v_vertical_02(table:list) -> str:
     x:int = 0
     bolinha:int = 0 
 
-    if table[0][2] == 1 and table[1][1] == 1 and table[2][0] == 1:
+    if table[0][2] == 'X' and table[1][1] == 'X' and table[2][0] == 'X':
         bolinha = 3
-    elif table[0][2] == 2 and table[1][1] == 2 and table[2][0] == 2:
+    elif table[0][2] == 'O' and table[1][1] == 'O' and table[2][0] == 'O':
         x = 3
     
     if bolinha == 3:
@@ -55,9 +55,9 @@ def v_lateal(table:list) -> str | bool:
 
         for j in range(size):
 
-            if table[i][j] == 2:
+            if table[i][j] == 'O':
                 bolinha += 1
-            elif table[i][j] == 1:
+            elif table[i][j] == 'X':
                 x += 1
 
             if bolinha == 3:
@@ -81,9 +81,9 @@ def v_horizontal(table:list) -> str | bool:
 
         for j in range(size):
 
-            if table[j][i] == 2:
+            if table[j][i] == 'O':
                 bolinha += 1
-            elif table[j][i] == 1:
+            elif table[j][i] == 'X':
                 x += 1
 
 
@@ -113,12 +113,12 @@ def insert_B(table:list, words)-> bool:
     size:int = data_int - 1
 
     if letter == 'A':
-        table[0][size] = 2
+        table[0][size] = 'O'
         
     elif letter == 'B':
-        table[0] [size] = 2
+        table[0] [size] = 'O'
     else:
-        table[0] [size] = 2
+        table[0] [size] = 'O'
 
 def insert_X(table:list, words)-> bool:
 
@@ -129,11 +129,19 @@ def insert_X(table:list, words)-> bool:
     size:int = data_int - 1
 
     if letter == 'A':
-        table[0][size] = 1
+        table[0][size] = 'X'
         
     elif letter == 'B':
-        table[0] [size] = 1
+        table[0] [size] = 'X'
     else:
-        table[0] [size] = 1
+        table[0] [size] = 'X'
 
     return True
+
+def print_table(table:list) -> None:
+
+    print(f"{table[0][0]} | {table[0][1]} | {table[0][2]}")
+    print("---------")
+    print(f"{table[1][0]} | {table[1][1]} | {table[1][2]}")
+    print("---------")
+    print(f"{table[2][0]} | {table[2][1]} | {table[2][2]}")
