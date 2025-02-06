116934aad7cf22bc46c311d5285b1da8f4dab9dc
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-02-06 09:54:23 -0300

    90% correção dos bugs

--- main.py
+++ main.py
@@ -1,145 +1,98 @@
 table: list = [
-#    a  b  c
-    [0, 0, 0], # 0
-    [0, 0, 0], # 1
-    [0, 8, 0]  # 2
+    [0,0,0], 
+    [0,0,0], 
+    [0,0,0] 
 ]
 
-
 def v_vertical_01(table:list) -> str:
-
     x:int = 0
     bolinha:int = 0 
-
     if table[0][0] == 'X' and table[1][1] == 'X' and table[2][2] == 'X':
-        bolinha = 3
-    elif table[0][0] == 'O' and table[1][1] == 'O' and table[2][2] == 'O':
         x = 3
-    
+    elif table[0][0] == 'O' and table[1][1] == 'O' and table[2][2] == 'O':
+        bolinha = 3
     if bolinha == 3:
-        return 'B'
+        return 'O'
     elif x == 3:
         return 'X'
     else:
         return False
 
 def v_vertical_02(table:list) -> str:
-
     x:int = 0
     bolinha:int = 0 
-
     if table[0][2] == 'X' and table[1][1] == 'X' and table[2][0] == 'X':
-        bolinha = 3
-    elif table[0][2] == 'O' and table[1][1] == 'O' and table[2][0] == 'O':
         x = 3
-    
+    elif table[0][2] == 'O' and table[1][1] == 'O' and table[2][0] == 'O':
+        bolinha = 3
     if bolinha == 3:
-        return 'B'
+        return 'O'
     elif x == 3:
         return 'X'
     else:
         return False
-
-#bolinha = 1
-# x = 2
-
+    
 def v_lateal(table:list) -> str | bool:
-
     bolinha:int = 0
     x:int = 0
     size:int = len(table)
-
     for i in range(size):
         x = 0
         bolinha = 0
-
         for j in range(size):
-
             if table[i][j] == 'O':
                 bolinha += 1
             elif table[i][j] == 'X':
                 x += 1
-
             if bolinha == 3:
-                return 'B'
+                return 'O'
             elif x == 3:
                 return 'X'
     return False
         
 def v_horizontal(table:list) -> str | bool:
-
     x:int = 0
     bolinha:int = 0
-
     count:int = 0
-
     size:int = len(table)
-
     for i in range(size):
         x = 0
         bolinha = 0
-
         for j in range(size):
-
             if table[j][i] == 'O':
                 bolinha += 1
             elif table[j][i] == 'X':
                 x += 1
-
-
             if x == 3:
                 return 'X'
             elif bolinha == 3:
-                return 'B'
-    
+                return 'O'
     return False
 
 def t_position(words) -> None:
-
     split = list(words)
     lette: str = split[0]
-    
     number:int = int(split[1])
     letter = lette.upper()
-
     return letter, number
 
 def insert_B(table:list, words)-> bool:
-
-    data:tuple = t_position(words)
-
-    letter:str = data[0]
-    data_int:int = data[1]
-    size:int = data_int - 1
-
-    if letter == 'A':
-        table[0][size] = 'O'
-        
-    elif letter == 'B':
-        table[0] [size] = 'O'
+    letter, number = t_position(words)
+    column = ord(letter) - ord('A')
+    if table [column][number - 1] == 'X' or table[column][number - 1] == 'O':
+        return False
     else:
-        table[0] [size] = 'O'
+        table[column][number - 1] = 'O'
 
 def insert_X(table:list, words)-> bool:
-
-    data:tuple = t_position(words)
-
-    letter:str = data[0]
-    data_int:int = data[1]
-    size:int = data_int - 1
-
-    if letter == 'A':
-        table[0][size] = 'X'
-        
-    elif letter == 'B':
-        table[0] [size] = 'X'
+    letter, number = t_position(words)
+    column = ord(letter) - ord('A')
+    if table[column][number - 1] == 'X' or table[column][number - 1] == 'O':
+        return False
     else:
-        table[0] [size] = 'X'
-
-    return True
+        table[column][number - 1] = 'X'
 
 def print_table(table:list) -> None:
-
     print("     1   2  3\n")
     print(f"A    {table[0][0]} | {table[0][1]} | {table[0][2]}")
     print("     ---------")
@@ -148,51 +101,69 @@ def print_table(table:list) -> None:
     print(f"C    {table[2][0]} | {table[2][1]} | {table[2][2]}")
 
 def all_v(table):
-
     v1 = v_vertical_01(table)
     v2 = v_vertical_02(table)
     v3 = v_horizontal(table)
     v4 = v_lateal(table)
-
     if v1 == "X" or v2 == "X" or v3 == "X" or v4 == "X":
         return "X"
-    elif v1 == "O" or v2 == "O" or v3 == "O" or v4 == "O":
-        return "B"
-
-
+    elif v1 == 'O' or v2 == 'O' or v3 == 'O' or v4 == 'O':
+        return "O"
+    else:
+        return False
+    
 def main():
-
-    rodados:int = 0
-
+    rodadas:int = 0
     print("\n--- Jogo da velha ---\n")
 
-    while rodados < 9:
-        
-        print("Jogador X:")
-        print_table(table)
+    while rodadas < 10:
 
+        print(f" Rodada {rodadas}/9")
+        print("\nJogador ( - X - ):")
+        print_table(table)
         j1 = input("\nEsclha uma posição: ") 
 
-        insert_X(table, j1)
+        v_x = insert_X(table, j1)
+        if v_x == False:
+            print("\n !!!  Posição já escolhida !!!\n ")
+            continue
 
+        rodadas += 1
+        result = all_v(table)
+        if result == 'X':
+            print ("\n --- X Venceu --- \n")
+            print_table(table)
 
-        print("jogador O:")
+            print("\n FIM DE JOGO \n!")
 
+            break
+
+        print(f"\n Rodada {rodadas}/9\n")
+        print("\njogador ( - O - ):")
         print_table(table)
 
         j2 = input("\n Escolha uma posição: \n\n")
+        v_b = insert_B(table, j2)
 
-        insert_B(table, j2)
+        if v_b == False:
+            print("\n !!! Posição já escolhida !!! \n")
+            continue
 
         print_table(table)
-
+        rodadas += 1
         result = all_v(table)
+        if result == 'O':
+            print ("\n --- Bolinha Venceu ---\n")
+            print_table(table)
 
-        if result == 'X':
-            print ("X Venceu")
+            print("\n FIM DE JOGO \n!")
             break
-        elif result == 'B':
-            print("Bolinha venceu")
+            
+        if rodadas == 9:
+            print("\n\n ---- Empate!! ---- \n")
+
+            print("\n FIM DE JOGO \n!")
+
             break
 
 main()
